8c68447abede92e93efb92aba09c2ba97c0d93e7
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-04-17 02:06:35 -0500

    add status, add pagination, add search for name

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -1,46 +1,57 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+const ResidentCard = ({ resident }) => {
+  const [residentInfo, setResidentInfo] = useState();
 
-const ResidentCard = ({resident}) => {
-
-    const [residentInfo, setResidentInfo] = useState()
-
-    useEffect(()  => {
-        axios.get(resident)
-            .then((res)  => setResidentInfo(res.data))
-            .catch((err)  => console.log(err))
-    },[])
+  useEffect(() => {
+    axios
+      .get(resident)
+      .then((res) => setResidentInfo(res.data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <article className='border-2 border-[#8EFF8B] w-[90%] md:w-[300px] md:h-full'>
-        <div>
-            <img className=' bg-cover w-full' src={residentInfo?.image} alt="" />
-        </div>
-    <section className='py-3 px-1'>
-        <h3 className='text-[28px] font-semibold ml-5'>{residentInfo?.name}</h3>
-        <hr className='border-1 border-[#084851] mb-3'/>
-        <ul className='ml-5'>
-            <li className='flex gap-3'>
-                <span className=' text-[#938686] text-[14px] w-[100px]'>Species</span>
-                <span>{residentInfo?.species}</span>
-            </li>
-            <li className='flex gap-3'>
-                <span className=' text-[#938686] text-[14px] w-[100px]'>Origin</span>
-                <span>{residentInfo?.origin.name}</span>
-            </li>
-            <li className='flex gap-3'>
-                <span className=' text-[#938686] text-[14px] w-[100px]'>Times appear</span>
-                <span>{residentInfo?.episode.length}</span>
-            </li>
-         
+    <article className="relative border-2 border-[#8EFF8B] w-[90%] md:w-[320px] ">
+      <div className="">
+        <img className=" bg-cover w-full" src={residentInfo?.image} alt="" />
+      </div>
+      <section className="py-3 px-1 ">
+        <h3 className="text-[28px] font-semibold ml-5">{residentInfo?.name}</h3>
+        <hr className="border-1 border-[#084851] mb-3" />
+        <ul className="ml-5 flex flex-col ">
+          <li className="flex gap-3">
+            <span className=" text-[#938686] text-[14px] w-[100px]">
+              Species
+            </span>
+            <span>{residentInfo?.species}</span>
+          </li>
+          <li className="flex gap-3">
+            <span className=" text-[#938686] text-[14px] w-[100px]">
+              Origin
+            </span>
+            <span>{residentInfo?.origin.name}</span>
+          </li>
+          <li className="flex gap-3">
+            <span className=" text-[#938686] text-[14px] w-[100px]">
+              Times appear
+            </span>
+            <span>{residentInfo?.episode.length}</span>
+          </li>
         </ul>
-        
-    </section>
+        <div className="flex justify-center items-center py-1 w-[180px] gap-4 border border-green absolute bottom-1/2 left-[20%] bg-[#020A02CC]">
+          {residentInfo?.status === "Dead" ? (
+            <div className=" bg-red-800 h-5 w-5 rounded-full"> </div>
+          ) : residentInfo?.status === "Alive" ? (
+            <div className=" bg-green h-5 w-5 rounded-full"> </div>
+          ) : (
+            <div className=" bg-gray-500 h-5 w-5 rounded-full"> </div>
+          )}
 
+          <span>{residentInfo?.status}</span>
+        </div>
+      </section>
     </article>
-    
-    
-  )
-}  
+  );
+};
 
-export default ResidentCard 
+export default ResidentCard;
--- src/components/ResidentList.jsx
+++ src/components/ResidentList.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
-import ResidentCard from './ResidentCard'
+import React, { useEffect, useState } from "react";
+import ResidentCard from "./ResidentCard";
 
-const ResidentList = ({location}) => {
+const ResidentList = ({ location }) => {
+  const residents = location?.residents;
+  const [currentPage, setcurrentPage] = useState(1);
 
-    const residents = location?.residents
+  let arrayPages = [];
+  const cantPage = Math.ceil(residents?.length / 20);
 
+  for (let i = 1; i <= cantPage; i++) {
+    arrayPages.push(i);
+  }
+
+  // 0 ,  20
+  // 20 , 40
+  // 40, 60
+
+  let start = currentPage * 20 - 20;
+  let end = currentPage * 20;
+
+  const changePage = (page) => {
+    window.scrollTo(0, 500);
+    setcurrentPage(page);
+  };
+
+  useEffect(()=> {
+    setcurrentPage(1)
+  }, [location])
   return (
-    <section className='flex flex-col items-center gap-4 md:flex-row md:flex-wrap md:justify-center'>
-        {
-            residents?.map((resident) => (
-                <ResidentCard key={resident} resident={resident}/>
-            ))}      
-    </section>
-  )
-}
+    <>
+      <section className="flex flex-col items-center gap-4 md:flex-row md:flex-wrap md:justify-center md:items-stretch  md:mb-10">
+        {residents?.slice(start, end).map((resident) => (
+          <ResidentCard key={resident} resident={resident} />
+        ))}
+      </section>
+      <div className="flex justify-center flex-wrap gap-3 mb-5 mt-3">
+        {arrayPages.map((page) => (
+          <div
+            key={page}
+            className={`h-[50px] w-[50px] lg:h-[80px]  lg:w-[80px] cursor-pointer hover:bg-[#062226] ${
+              currentPage === page && "bg-[#062226]"
+            } flex justify-center items-center`}
+            onClick={() => changePage(page)}
+          >
+            <p className="text-[22px] lg:text-[25px]">{page}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default ResidentList
+export default ResidentList;
